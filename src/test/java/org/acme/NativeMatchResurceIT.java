d58d5a18037517a3f53e05aef4fcaeb15d582e2d
MishaSm <mishasmirnow2011@mail.ru>
2019-07-16 16:10:59 +0300

    Reset
    reset repo

--- src/test/java/org/acme/NativeMatchResurceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeMatchResurceIT extends MatchResurceTest {
-
-    // Execute the same tests but in native mode.
-}
